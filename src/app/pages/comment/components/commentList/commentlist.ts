907ce1677f0a16d0481d4e0c069c900efc04f2de
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-15 17:41:25 +0200

    ...

--- src/app/pages/comment/components/commentList/commentlist.ts
+++ src/app/pages/comment/components/commentList/commentlist.ts
@@ -74,7 +74,7 @@ export class commentlistComponent implements OnChanges, OnInit {
     this.timelineCommunicationService.getPChanges(this.currentTitle.id).subscribe((res2) => {
       //this.doChange(res2);
       this.commandTidslinjeWrapper = res2;
-      this.commandTidslinjeWrapperFun();
+      this.changeCommandTidslinjeWrapper();
       return;
 
 
@@ -87,14 +87,6 @@ export class commentlistComponent implements OnChanges, OnInit {
     
       
     }
-  }
-  //Get change in start and end of selection of text
-  //@Input('selectStart') selectStart: Number = new Number();
-  @Output() selectStartChange: EventEmitter<Number> = new EventEmitter<Number>();
-
-  async selectStartChangeFun() {
-    
-    this.selectStartChange.emit(this.selectStart);
   }
 
   ishighlighting = false;
@@ -167,7 +159,7 @@ export class commentlistComponent implements OnChanges, OnInit {
       this.timelineCommunicationService.getPChanges(this.currentTitle.id).subscribe((res2) => {
         //this.doChange(res2);
         this.commandTidslinjeWrapper = res2;
-        this.commandTidslinjeWrapperFun();
+        this.changeCommandTidslinjeWrapper();
         return;
 
 
@@ -175,13 +167,15 @@ export class commentlistComponent implements OnChanges, OnInit {
     })}
 
   
-  
+  changeCommandTidslinjeWrapper() {
+    this.timelineDataStorageService.changecommandTidslinjeWrapper(this.commandTidslinjeWrapper)
+  }
   async removeById(id: Number) {
     this.timelineCommunicationService.removePTimeLineById(id).subscribe((res) => {
       console.log("leaved remove service")
       this.timelineCommunicationService.getPChanges(this.currentTitle.id).subscribe((res2) => {
         this.commandTidslinjeWrapper = res2;
-        this.commandTidslinjeWrapperFun();
+        this.changeCommandTidslinjeWrapper();
         return;
 
 
@@ -190,61 +184,5 @@ export class commentlistComponent implements OnChanges, OnInit {
 
  
   }
-  //@Input('selectEnd') selectEnd: Number = new Number();
 
-  @Output() selectEndChange: EventEmitter<Number> = new EventEmitter<Number>();
-  async selectEndChangeFun() {
-    this.selectEndChange.emit(this.selectEnd);
-  }
-
-
-  //Send selected text between child components
-  //@Input('selectedText') selectedText: String = new String();
-  @Output() selectedTextChange: EventEmitter<String> = new EventEmitter<String>();
-
-  async selectedTextChangeFun() {
-    this.selectedTextChange.emit(this.selectedText);
-  }
-
-
-  //Changes from server conserning comments
-  //@Input('commandTidslinjeWrapper') commandTidslinjeWrapper: Array<tidslinjeCommandWrapper> = new Array<tidslinjeCommandWrapper>();
-  @Output() commandTidslinjeWrapperChange: EventEmitter<Array<tidslinjeCommandWrapper>> = new EventEmitter<Array<tidslinjeCommandWrapper>>();
-
-  async commandTidslinjeWrapperFun() {
-    this.commandTidslinjeWrapperChange.emit(this.commandTidslinjeWrapper);
-  }
-
-
-  //When choosen a title, send timelines here
-  //@Input('tidslinjerList') tidslinjerList: Array<tidslinje> = new Array<tidslinje>();
-  @Output() tidslinjerListChange: EventEmitter<Array<tidslinje>> = new EventEmitter<Array<tidslinje>>();
-
-  async tidslinjerListChangeFun() {
-    this.tidslinjerListChange.emit(this.tidslinjerList);
-  }
- 
-  //When entering website, load all titles.
-  //@Input('titleList') titleList: Array<String> = new Array<String>();
-  @Output() titleListChange: EventEmitter<Array<String>> = new EventEmitter<Array<String>>();
-
-  async titleListChangeFun() {
-    this.titleListChange.emit(this.titleList);
-  }
-
-  //Current title
-  //@Input('currentTitle') currentTitle: title = new title();
-  @Output() currentTitleChange: EventEmitter<title> = new EventEmitter<title>();
-
-  async titleChangeFun() {
-    this.currentTitleChange.emit(this.currentTitle);
-  }
-
-  //Filtered timelines
-  //  @Input('filteredtimelines') filteredtimelines: Observable<Array<tidslinje>> = new Observable<Array<tidslinje>>();
-  @Output() filteredtimelinesChange: EventEmitter<Observable<Array<tidslinje>>> = new EventEmitter<Observable<Array<tidslinje>>>();
-
-  async filteredTimelinesChangeFun() {
-    //this.filteredtimelinesChange.emit(this.filteredtimelines);
-  }
 }
